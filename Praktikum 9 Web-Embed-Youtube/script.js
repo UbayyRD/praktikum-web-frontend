5a3d43577d53c634ec6248c7cbaa9574ecc29758
UbayyRD <ubayrafif10@gmail.com>
2025-03-15 11:17:03 +0700

    Latihan 2

--- Praktikum 9 Web-Embed-Youtube/script.js
+++ Praktikum 9 Web-Embed-Youtube/script.js
@@ -1,9 +1,4 @@
 function changeVideo(videoId) {
     let youtubeFrame = document.getElementById("youtubeVideo");
-    youtubeFrame.src = `https://www.youtube.com/embed/${TEgBVjbdrl0}`;
-}
-
-function changeVideo(videoId) {
-    let youtubeFrame = document.getElementById("youtubeVideo");
-    youtubeFrame.src = `https://www.youtube.com/embed/${EwlJvNjV6SQ}`;
+    youtubeFrame.src = `https://www.youtube.com/embed/${videoId}`;
 }
