edd5ed089f1191882ec5d2ac02cae4c22174f0b7
UbayyRD <ubayrafif10@gmail.com>
2025-03-15 09:03:21 +0700

    Praktikum 9 Web-Embed-Youtube

---
+++ Praktikum 9 Web-Embed-Youtube/script.js
@@ -0,0 +1,9 @@
+function changeVideo(videoId) {
+    let youtubeFrame = document.getElementById("youtubeVideo");
+    youtubeFrame.src = `https://www.youtube.com/embed/${TEgBVjbdrl0}`;
+}
+
+function changeVideo(videoId) {
+    let youtubeFrame = document.getElementById("youtubeVideo");
+    youtubeFrame.src = `https://www.youtube.com/embed/${EwlJvNjV6SQ}`;
+}
